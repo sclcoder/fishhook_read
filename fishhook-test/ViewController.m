6fde3b16c76bf6031c305caca8597a198bff011f
scl <sclcoder@163.com>
2020-09-29 16:16:43 +0800

    深入理解fishhook

--- fishhook-test/ViewController.m
+++ fishhook-test/ViewController.m
@@ -15,25 +15,6 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    NSLog(@"NSLog符号还没被绑定");
-    
-    NSLog(@"NSLog被绑定了");
-    
-
-    struct rebinding nslog;
-    nslog.name = "NSLog";
-    nslog.replacement = mylog;
-    nslog.replaced = (void *)&syslog;
-    
-    /// rebinding结构体数组
-    struct rebinding rebs[1] = {nslog};
-    /// 重绑定
-    rebind_symbols(rebs, 1);
-}
-
 /************** 更改NSLog*************/
 
 /// 函数指针：用于接收NSLog函数地址
@@ -46,9 +27,27 @@ void mylog(NSString *format, ...){
 }
 
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    struct rebinding nslog;
+    nslog.name = "NSLog";
+    nslog.replacement = mylog;
+    nslog.replaced = (void *)&syslog; /// 传入的是个函数指针的地址,最终接收一个函数的地址
+    
+    /// rebinding结构体数组
+    struct rebinding rebs[1] = {nslog};
+    /// 重绑定
+    rebind_symbols(rebs, 1);
+    
+    NSLog(@"NSLog被hook了--");
+    NSLog(@"NSLog被hook了--");
+}
+
+
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
-    NSLog(@"NSLog被hook了");
+    NSLog(@"NSLog被hook了--");
 }
 
 
--- fishhook-test/fishhook/fishhook.c
+++ fishhook-test/fishhook/fishhook.c
@@ -30,6 +30,9 @@
 #include <mach-o/dyld.h>
 #include <mach-o/loader.h>
 #include <mach-o/nlist.h>
+#include <stdio.h>
+
+
 
 #ifdef __LP64__
 typedef struct mach_header_64 mach_header_t;
@@ -102,19 +105,28 @@ static int prepend_rebindings(struct rebindings_entry **rebindings_head,
  2.另外除了__la_symbol_prt表之外，还有与之对应的indirect Symbols、Symbol Table以及String Table。
    它们之间的关系是，首先通过符号在__la_symbol_prt的index，加上在Load Command中对__la_symbol_prt的描述信息reversed1，找到indirect Symbols中 index+reversed1 位置的数据index2，然后在找到Symbol Table中index2位置的数据拿到偏移地址offset，最后在String Table中找offset处的数据，该数据就是函数名"_NSLog"
 
-    作者：初心丶可曾记
-    链接：https://www.jianshu.com/p/54946d7e8d36
  */
 static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
-                                           section_t *section,
+                                           section_t *section, //
                                            intptr_t slide,
                                            nlist_t *symtab,
                                            char *strtab,
                                            uint32_t *indirect_symtab) {
-  //indirect Symbols中包含了各个section中符号在Symbol Table的index，这里是直接定位到对应__DATA,__la_symbol_ptr所在的index数组
+    
+  /**
+    0. 在Section64 Header(__lay_symbol_ptrs)和Section64 Header(got)中的reserved1中记录着section中的符号其在indirect_symtab表中起始index
+   
+    1. 这里是通过indirect_symtab地址 + 对应section->reserved1可以定位到对应section中的符号在indirect_symtab表中的数组地址
+       这里的section从fishhook的源码来看指的是(__DATA,__la_symbol_ptrs) 、 (__DATA_const,got)这两个数据节
+   
+    2. 在indirect Symbols中包含了各个section中符号在Symbol Table的index
+   */
+  
+  // 对应section中符号的在Symtab Table中的index数组
   uint32_t *indirect_symbol_indices = indirect_symtab + section->reserved1;
   
- // slide + section->addr得到了符号指针表__la_symbol_ptr的实际地址，即可以获得函数实现的地址
+  //slide + section->addr得到了符号指针表(__DATA,__la_symbol_ptrs) or (__DATA_const,got)的实际地址
+  // (__DATA,__la_symbol_ptrs) or (__DATA_const,got) 存放的是指针的指针（没有指定类型）即指针数组--函数的地址数组
   void **indirect_symbol_bindings = (void **)((uintptr_t)slide + section->addr);
     
   for (uint i = 0; i < section->size / sizeof(void *); i++) {
@@ -126,7 +138,9 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
       continue;
     }
 
+      
 // This is the symbol table entry structure for 64-bit architectures.
+      
 // symbol表结构
 //     struct nlist_64 {
 //         union {
@@ -143,24 +157,29 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
       
     //String Table的起始地址 + 偏移量，得到符号的名称
     char *symbol_name = strtab + strtab_offset; /// 获取的值是二进制数据,使用char *取值便可以得到字符串
+      
+
     // 判断函数名称是否有两个字符 为什么要这么判断 因为函数前有个_,所以函数名称至少有一个字符
+      /**
+       调试信息
+         (lldb) p symbol_name
+         (char *) $4 = 0x0000000105a713ad "_NSLog"
+         (lldb) p symbol_name[1]
+         (char) $5 = 'N'
+         (lldb) p symbol_name[0]
+         (char) $6 = '_'
+       */
     bool symbol_name_longer_than_1 = symbol_name[0] && symbol_name[1];
       
-   /**
-    调试信息
-      (lldb) p symbol_name
-      (char *) $4 = 0x0000000105a713ad "_NSLog"
-      (lldb) p symbol_name[1]
-      (char) $5 = 'N'
-      (lldb) p symbol_name[0]
-      (char) $6 = '_'
-    */
+    /** 以上操作就找到了 (__DATA,__la_symbol_ptrs---懒加载符号指针表) or (__DATA_const,got---非懒加载符号指针边)中的实际符号
+        注意 (__DATA,__la_symbol_ptrs) , (__DATA_const,got)存放的并不是符号的名字
+     **/
+      
       
     // 遍历最初链表，依次进行hook
     struct rebindings_entry *cur = rebindings;
     while (cur) {
       for (uint j = 0; j < cur->rebindings_nel; j++) {
-          
         /// strcmp用来判断 &symbol_name[1]与rebindings中对应的函数名是否相等 相等即为目标hook函数
           
           // 调试信息
@@ -169,31 +188,56 @@ static void perform_rebinding_with_section(struct rebindings_entry *rebindings,
                
                // (lldb) p &symbol_name[1]
                // (char *) $10 = 0x0000000105a713ae "NSLog"
+
         if (symbol_name_longer_than_1 &&
             strcmp(&symbol_name[1], cur->rebindings[j].name) == 0) {
         
           //编译的时候会将符号转成带下划线的，比如printf会转成_printf,所以从下划线后面的字符开始比较
           if (cur->rebindings[j].replaced != NULL &&
               indirect_symbol_bindings[i] != cur->rebindings[j].replacement) {
-            //保存原来的符号地址
-            *(cur->rebindings[j].replaced) = indirect_symbol_bindings[i];
               
+            // 记录函数地址
+            *(cur->rebindings[j].replaced) = indirect_symbol_bindings[i];
+            /**
+             我对fishhook最大的疑问?
+                 *(cur->rebindings[j].replaced) = indirect_symbol_bindings[i]
+             该操作是将__DATA.__la_symbol_ptr 和 __DATA.__la_symbol_ptr 的 Indirect Pointer值赋值给*(cur->rebindings[j].replaced)
+             那么问题来了,当第一次使用懒加载符号时对应的indirect_symbol_bindings[i]中存放的是 __stub_helper的执行地址。即*(cur->rebindings[j].replaced)保存的是__stub_helper的执行地址。
+             
+             通过调试发现 我们需要hook的函数被其他image调用了，且该image的懒加载表或非懒加载表里面已经是真实的函数地址，那么fishhook在处理的时候，虽然第一次是拿到了我们image内指向stub_helper的地址，但是后面会被其他image里面的真实的函数地址所覆盖。所以调用原函数指针对应的函数并不影响我们image里面的懒加载表。 因为fishhook的通过_dyld_register_func_for_add_image注册了image加载回调。
+             但是还是不能保证，我们通过*(cur->rebindings[j].replaced)记录的hook的函数符号有正确的地址，所以最好在hook函数前，先将该函数调用一下，保证改函数符号已经绑定了正确的地址
+             
+             参考:
+             https://www.jianshu.com/p/828ec78d4ae1
+             http://m.desgard.com/2017/12/17/fishook-1/index.html
+             https://www.jianshu.com/p/4b32e8c54389
+             */
           }
-          // 替换__DATA,__la_symbol_ptr表中的符号地址
+          // 替换符号指针表中的地址
           indirect_symbol_bindings[i] = cur->rebindings[j].replacement;
-          //退出链表的遍历，即针对同一符号多次调用fishhook重绑定，只有会对最后一次调用的生效
+            
+          // 退出链表的遍历，即针对同一符号多次调用fishhook重绑定，只有会对最后一次调用的生效
           goto symbol_loop;
         }
       }
       cur = cur->next;
+        
     }
   symbol_loop:;
   }
 }
 
+/**
+    这段方法主要描述了替换 __DATA.__la_symbol_ptr 和 __DATA.__la_symbol_ptr 的 Indirect Pointer 主要过程。从 reserved1 字段获取到 Indirect Symbols 对应的位置。从中我们可以获取到指定符号的偏移量，这个偏移量主要用来在 String Table 中检索出符号名称字符串。之后我们找到 __DATA.__la_symbol_ptr 和 __DATA.__la_symbol_ptr 这两个 Section。这两个表中，都是由 Indirect Pointer 构成的指针数组，但是其中的元素决定了我们调用的方法应该以哪个代码段的方法来执行。我们遍历这个指针数组中每一个指针，在每一层遍历中取出其符号名称，与我们的 rebindings 链表中每一个元素进行比对，当名称匹配的时候，重写其指向地址。
+ 
+ */
+
 static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
                                      const struct mach_header *header,
                                      intptr_t slide) {
+    
+    printf("header: %p\n", header);
+    
   // dladdr函数作用: 在程序中查找header
   Dl_info info;
   if (dladdr(header, &info) == 0) {
@@ -228,7 +272,7 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
     //第一次遍历所有的LoadCommand
   for (uint i = 0; i < header->ncmds; i++, cur += cur_seg_cmd->cmdsize) {
     
-    cur_seg_cmd = (segment_command_t *)cur; //保存当前的LoadCommand
+    cur_seg_cmd = (segment_command_t *)cur; // 保存当前的LoadCommand
       
     if (cur_seg_cmd->cmd == LC_SEGMENT_ARCH_DEPENDENT) { /// for 64-bit architectures
         
@@ -255,13 +299,15 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
 
   // Find base symbol/string table addresses
   
-    //slide是ASLR的随机偏移，linkedit_segment->vmaddr - linkedit_segment->fileoff是mach-o在文件中的基地址，两者相加就是ASLR后的mach-o加载进内存的基地址
+  // slide是ASLR的随机偏移，linkedit_segment->vmaddr - linkedit_segment->fileoff是"mach-o"在文件中的基地址，两者相加就是ASLR后的mach-o加载进内存的基地址
     
   // 链接时程序的基址 = __LINKEDIT.VM_address - __LINKEDIT.File_offset + slide的改变值    (跳过了LC_SEGMENT_64(__PAGEZERO)这个Command)
     
   uintptr_t linkedit_base = (uintptr_t)slide + linkedit_segment->vmaddr - linkedit_segment->fileoff;
+    
   // Symbol Table地址 = 基址 + 符号表偏移量
   nlist_t *symtab = (nlist_t *)(linkedit_base + symtab_cmd->symoff);
+    
   // String Table地址 = 基址 + 字符串表偏移量
   char *strtab = (char *)(linkedit_base + symtab_cmd->stroff);
 
@@ -271,6 +317,7 @@ static void rebind_symbols_for_image(struct rebindings_entry *rebindings,
 
   //指针指向LoadCommand起始地址
   cur = (uintptr_t)header + sizeof(mach_header_t);
+    
   //第二次遍历所有的LoadCommand
   for (uint i = 0; i < header->ncmds; i++, cur += cur_seg_cmd->cmdsize) {
     
@@ -331,6 +378,7 @@ static void _rebind_symbols_for_image(const struct mach_header *header,
     // 第二个参数 header是 当前 image 的头信息，
     // 第三个参数 slide是 ASLR 的偏移
     rebind_symbols_for_image(_rebindings_head, header, slide);
+    
 }
 
 int rebind_symbols_image(void *header,
